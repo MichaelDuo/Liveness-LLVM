42e41d3069d43cd78060e94527314772cfbd913e
Michael Dong <michael@Michaels-MacBook-Pro.local>
2020-03-07 14:29:51 -0800

    finish implementation

--- Pass/Transforms/Liveness/Liveness.cpp
+++ Pass/Transforms/Liveness/Liveness.cpp
@@ -5,9 +5,13 @@
 #include "llvm/IR/Type.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/Instruction.h"
+#include "llvm/IR/CFG.h"
+#include "llvm/IR/BasicBlock.h"
 #include <string>
-#include <map>
+#include <vector>
 #include <set>
+#include <unordered_map>
+#include <algorithm>
 
 using namespace llvm;
 using namespace std;
@@ -24,24 +28,52 @@ struct ValueNumbering : public FunctionPass {
 
   void findVarKillAndUEVar(map<BasicBlock*, map<string, set<Value*>>> &table, BasicBlock &bb){
     for(auto& inst: bb){
-      if(inst.isBinaryOp()){
+      map<string, set<Value*>> *bbInfo = &table[&bb];
+      set<Value*> *VarKill = &(*bbInfo)["VarKill"];
+      set<Value*> *UEVar = &(*bbInfo)["UEVar"];
+
+      if(inst.getOpcode()==31){
+        Value* v = inst.getOperand(0);
+        if(VarKill->find(v) == VarKill->end()){
+          UEVar->insert(v);
+        }
+      }
+
+      if(inst.getOpcode()==32){
+        VarKill->insert(inst.getOperand(1));
+      }
+    }
+
+  }
+
+  void findLiveout(map<BasicBlock*, map<string, set<Value*>>> &table, Function &F){
+    bool flag = true;
+    while(flag){
+      flag = false;
+      for(auto& bb: F){
+
+        set<Value*> dest, dest1;
+        
         map<string, set<Value*>> *bbInfo = &table[&bb];
+        set<Value*> prev = (*bbInfo)["LiveOut"];
         set<Value*> *VarKill = &(*bbInfo)["VarKill"];
         set<Value*> *UEVar = &(*bbInfo)["UEVar"];
+        set<Value*> *LiveOut = &(*bbInfo)["LiveOut"];
 
-        Value* v1 = inst.getOperand(0);
-        Value* v2 = inst.getOperand(1);
+        for(BasicBlock* Succ: successors(&bb)){
+          map<string, set<Value*>> *SuccBbInfo = &table[Succ];
+          set<Value*> *SuccVarKill = &(*SuccBbInfo)["VarKill"];
+          set<Value*> *SuccUEVar = &(*SuccBbInfo)["UEVar"];
+          set<Value*> *SuccLiveOut = &(*SuccBbInfo)["LiveOut"];
 
-        StringRef varName = v1->getName();
-
-        if(VarKill->find(v1) == VarKill->end()){
-          UEVar->insert(v1);
+          set_difference(SuccLiveOut->begin(), SuccLiveOut->end(), SuccVarKill->begin(), SuccVarKill->end(), inserter(dest, dest.begin()));
+          set_union(dest.begin(), dest.end(), SuccUEVar->begin(), SuccUEVar->end(), inserter(dest1, dest1.begin()));
+          set_union(dest1.begin(), dest1.end(), LiveOut->begin(), LiveOut->end(), inserter(*LiveOut, LiveOut->begin()));
+          dest.clear(); dest1.clear();
         }
-        if(VarKill->find(v2) == VarKill->end()){
-          UEVar->insert(v2);
+        if(!std::equal(prev.begin(), prev.end(),LiveOut->begin(), LiveOut->end())){
+          flag = true;
         }
-
-        VarKill->insert(&inst);
       }
     }
   }
@@ -66,24 +98,39 @@ struct ValueNumbering : public FunctionPass {
 
     for(auto& basic_block: F){
       findVarKillAndUEVar(table, basic_block);
-
-      map<string, set<Value*>> bbInfo = table[&basic_block];
-      set<Value*> VarKill = bbInfo["VarKill"];
-      set<Value*> UEVar = bbInfo["UEVar"];
     }
+
+    findLiveout(table, F);
 
     for(auto& basic_block: F){
-      errs() << "Basic Block: \n";
+      errs() << "----- ";
+      basic_block.printAsOperand(errs(), false);
+      errs() << " -----\n";
       map<string, set<Value*>> bbInfo = table[&basic_block];
       set<Value*> VarKill = bbInfo["VarKill"];
       set<Value*> UEVar = bbInfo["UEVar"];
+      set<Value*> LiveOut = bbInfo["LiveOut"];
+
+      errs() << "UEVar: ";
       for(auto it = UEVar.begin();it != UEVar.end();++ it){
-        errs() << **it << "\n";
+        errs() << (**it).getName() << " ";
       }
+      errs() << "\n";
+
+      errs() << "VarKill: ";
+      for(auto it = VarKill.begin();it != VarKill.end();++ it){
+        errs() << (**it).getName() << " ";
+      }
+      errs() << "\n";
+
+
+      errs() << "LiveOut: ";
+      for(auto it = LiveOut.begin();it != LiveOut.end();++ it){
+        errs() << (**it).getName() << " ";
+      }
+      errs() << "\n";
+
     }
-
-
-
     
     return false;
   }
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,5 @@
 cd ./Pass/build && cmake -DCMAKE_BUILD_TYPE=Release ../Transforms/Liveness && make -j4 && cd -
 
-fileName=1
+fileName=2
 cd test 
-clang -Xclang -disable-O0-optnone $fileName.c -O0 -S -emit-llvm -o $fileName.ll 
-opt $fileName.ll -mem2reg -S -o $fileName.bc
-opt -load ../Pass/build/libLLVMLivenessPass.so  -ValueNumbering < $fileName.bc > /dev/null 
-# opt -load ../Pass/build/libLLVMLivenessPass.so  -ValueNumbering < $fileName.bc > /dev/null 2> $fileName.out
+opt -load ../Pass/build/libLLVMLivenessPass.so  -ValueNumbering < $fileName.ll > /dev/null 
